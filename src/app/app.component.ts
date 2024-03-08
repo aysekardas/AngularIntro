70dfe498c88bde5108f0cf9a49e58c033fdc2c9e
aysekardas <aysekardas7@gmail.com>
2024-03-09 00:18:59 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
+
+// annotation, attribute, decorator
+@Component({
+  selector: 'app-root', // <app-root> <app-root/>
+  standalone: true, // sonradan bakılacak.
+  imports: [RouterOutlet, FormComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'Tobeto';
+  count = 0;
+  inputValue: string = '';
+  onBtnClick(event: Event) {
+    console.log(this.inputValue);
+    this.count++;
+  }
+  onChange(event: Event) {
+    let element = event.target as HTMLInputElement;
+    console.log('Input değeri değişti', element.value);
+    this.inputValue = element.value;
+  }
+}
