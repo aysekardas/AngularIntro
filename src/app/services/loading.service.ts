2c4e1903618d45540da42b49e28b5e30013830d5
aysekardas <aysekardas7@gmail.com>
2024-03-13 23:52:16 +0300

    fix loading code smell

--- src/app/services/loading.service.ts
+++ src/app/services/loading.service.ts
@@ -4,10 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class LoadingService {
-  isLoading: boolean = false;
+  requestCount: number = 0;
   constructor() {}
 
-  setLoading(value: boolean) {
-    this.isLoading = value;
+  addRequest() {
+    this.requestCount++;
+  }
+  removeRequest() {
+    this.requestCount--;
   }
 }
+// requestCount + -
+// isLoading true false
